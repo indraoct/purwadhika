2dbc692a65762236ef482cb6051f741699182333
Indra Octama <indra.octama@orori.com>
2018-10-24 10:22:55 +0700

    update number 8

--- h1/number_8.js
+++ h1/number_8.js
@@ -6,21 +6,22 @@ let start_hour = 9; //waktu berangkat
 /**
  * yang dicari adalah jarak antara a & b = 0 (tabrakan)
  * jika jarak = kec * waktu
- * dan x = waktu
+ * dan t = waktu
  * maka 
- *    120 km - ((60km/jam) * x jam) + (40km/jam) * x jam)) = 0 
- * <=> 120 km - (60km/jam *x jam  + 40km/jam *x jam ) = 0
- * <=> 120 km = (60 * x jam) km + (40 * x jam) km
- *    x  = (120/100) jam
+ *    s - ( av * x jam) + (bv * x jam)) = 0 
+ * <=> s km - (avkm/jam * t jam  + bv km/jam *x jam ) = 0
+ * <=> s km = (av * t jam) km + (40 * t jam) km
+ * <=> s km = (av + bv )t * jam * km
+ *     t  = s/(av + bv)  jam
  */
 
 //waktu
- var x = 120/100;
- console.log("waktu yang dibutuhkan = "+x+" jam");
+ var t = s/(av+bv);
+ console.log("waktu yang dibutuhkan = "+t+" jam");
  
  //bertabrakan pada saat
- var y = start_hour + x;
- console.log("jam tabrakan = "+Math.floor(y)+" menit ke "+(60*(y - Math.floor(y))));
+ var y = start_hour + t;
+ console.log("tabrakan pada saat jam = "+Math.floor(y)+", menit ke "+Math.round(60*(y - Math.floor(y))));
 
 
 
