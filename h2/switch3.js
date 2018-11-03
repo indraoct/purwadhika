ff1a8bf7e63e4ae0d49db9c77a8dfa06b387076c
Indra Octama <indra.octama@orori.com>
2018-11-03 14:55:59 +0700

    switch 3

---
+++ h2/switch3.js
@@ -0,0 +1,40 @@
+let tgl = new Date();
+
+switch (tgl.getMonth()){
+    case 0:
+        console.log("ini bulan "+tgl.getMonth()+" : Januari");
+    break;
+    case 1:
+        console.log("ini bulan "+tgl.getMonth()+" : Februari");
+    break;
+    case 2:
+        console.log("ini bulan "+tgl.getMonth()+" : Maret");
+    break;
+    case 3:
+        console.log("ini bulan "+tgl.getMonth()+" : April");
+    break;
+    case 4:
+        console.log("ini bulan "+tgl.getMonth()+" : Mei");
+    break;
+    case 5:
+        console.log("ini bulan "+tgl.getMonth()+" : Juni");
+    break;
+    case 6:
+        console.log("ini bulan "+tgl.getMonth()+" : Juli");
+    break;
+    case 7:
+        console.log("ini bulan "+tgl.getMonth()+" : Agustus");
+    break;
+    case 8:
+        console.log("ini bulan "+tgl.getMonth()+" : September");
+    break;
+    case 9:
+        console.log("ini bulan "+tgl.getMonth()+" : Oktober");
+    break;
+    case 10:
+        console.log("ini bulan "+tgl.getMonth()+" : November");
+    break;
+    case 11:
+        console.log("ini bulan "+tgl.getMonth()+" : Desember");
+    break;
+}
