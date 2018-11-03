1cbc74e84181bdc646bf279e5b41fc445afd42a1
Indra Octama <indra.octama@orori.com>
2018-11-03 14:40:52 +0700

    h2 nomer 1

---
+++ h2/switch1.js
@@ -0,0 +1,33 @@
+let alas = "sandal"
+let warna = "biru"
+let harga = "murah"
+
+switch (true){
+    case alas == "sandal" && warna == "merah" && harga == "murah":
+        console.log("Saya suka sandal merah murah");
+    break;
+    case alas == "sandal" && warna == "merah" && harga == "mahal":
+        console.log("Saya suka sandal merah mahal");
+    break;
+    case alas == "sandal" && warna == "biru" && harga == "mahal":
+        console.log("Saya suka sandal biru mahal");
+    break;
+    case alas == "sandal" && warna == "biru" && harga == "murah":
+        console.log("Saya suka sandal biru murah");
+    break;
+
+    case alas == "sepatu" && warna == "merah" && harga == "murah":
+        console.log("Saya suka sepatu merah murah");
+    break;
+    case alas == "sepatu" && warna == "merah" && harga == "mahal":
+        console.log("Saya suka sepatu merah mahal");
+    break;
+    case alas == "sepatu" && warna == "biru" && harga == "mahal":
+        console.log("Saya suka sepatu biru mahal");
+    break;
+    case alas == "sepatu" && warna == "biru" && harga == "murah":
+        console.log("Saya suka sepatu biru murah");
+    break;
+    default:
+        console.log("saya suka nyeker");
+}
