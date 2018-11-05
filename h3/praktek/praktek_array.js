e32bd1e6851339be1004016c9a07c18f332a5385
Indra Octama <indra.octama@orori.com>
2018-11-05 09:59:44 +0700

    praktek h3

---
+++ h3/praktek/praktek_array.js
@@ -0,0 +1,28 @@
+let buah = [];
+
+buah[0] = "Nanas";
+buah[1] = "Strawberry";
+buah[2] = "Tomat";
+buah[3] = "Wortel";
+
+//buah.pop();
+//buah.shift();
+
+let y= buah.indexOf("Wortel");
+
+//console.log(y);
+
+//console.log(buah.length);
+
+for (i = 0; i < buah.length; i++){
+    //console.log(" Nama buah index ke "+i+" adalah "+buah[i]);
+}
+
+//array multi dimensi 
+
+let student = [];
+student[0] = [1,"Indra "];
+student[1] = [2,"Sugi","Karawaci"]; 
+student[2] = [3,"Meidi"];
+
+console.log(student);
